c972b3e1fbc3e76a467886db7b6c824f20328e30
ahtrahdis7 <sidharthabiki@gmail.com>
2021-08-22 12:08:10 +0530

    add: color and functionality

--- components/TextInput.jsx
+++ components/TextInput.jsx
@@ -3,55 +3,85 @@ import { Pressable, View, TextInput, StyleSheet, Text } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 
-// const storeData = async (key, time, value, eff) => {
-//     try {
-//         await AsyncStorage.setItem(key, value)
-//     } catch (e) {
-//         // saving error
-//     }
-// }
 
-// const handleData = (key, time, input, eff) => {
-    
-// }
-
-
+/**
+ *
+ *
+ * @export
+ * @param {*} props
+ * @return {*} 
+ */
 export default function CustomTextInput(props){
-    // const [inputTime, onChangeinputTime] = React.useState(props.time);
-    const [inputTask, onChangeinputTask] = React.useState("...task");
-    const [inputEff, onChangeinputEff] = React.useState("0");
+    const [ inputTask, onChangeinputTask ] = React.useState("...task");
+    const [ inputEff, onChangeinputEff ] = React.useState("0");
+    const [ COLOR, SET_COLOR ] = React.useState("#e6e6e6");
+    const [ EFF_COLOR, SET_EFF_COLOR ] = React.useState("#ff7a7a");
     let key = props.date
     , inputTime = props.time;
-    // console.log(props);
-    const onPressDone = async () => {
-        if(await AsyncStorage.getItem(key) !== null){
+    React.useEffect(() => {
+        /**
+         * This function is used to do asuncronous operation inside the
+         * UseEffect Hook. Without doing so, was fetching me an error.
+         */
+        async function getPrevData(){
             let prevData = await AsyncStorage.getItem(key);
-            // prevData/
             prevData = JSON.parse(prevData);
-            prevData[inputTime] = { task: inputTask, eff: inputEff }
-            
-            let eff = 0;
+            onChangeinputEff(prevData[inputTime] != undefined ? prevData[inputTime].eff: "0");
+            onChangeinputTask(prevData[inputTime] != undefined ? prevData[inputTime].task: "...task");
+            if(inputTask !== "...task" ) SET_COLOR("#8affa9");
+            if(inputTime >= parseInt(7) ) SET_EFF_COLOR("#8affa9");
+            else if(inputTime >= 4) SET_EFF_COLOR("#ffbd7a")
+            else SET_EFF_COLOR("#ff7a7a")
+        };
+        getPrevData();
+        
+    },[]);
 
-            for(let entry in prevData){
-                
-                if(entry !== "eff"){
-                    // console.log(parseInt(prevData[entry].eff)/14.0);
-                    eff += parseInt(prevData[entry].eff)/14.0;
+    React.useEffect(() => {
+        /**
+         * This function is used to do asuncronous operation inside the
+         * UseEffect Hook. Without doing so, was fetching me an error.
+         */
+        async function asuncFunction(){
+            if(await AsyncStorage.getItem(key) !== null){
+                let prevData = await AsyncStorage.getItem(key);
+                prevData = JSON.parse(prevData);
+                prevData[inputTime] = { task: inputTask, eff: inputEff }
+                let eff = 0;
+                for(let entry in prevData){
+                    if(entry !== "eff"){
+                        eff += parseInt(prevData[entry].eff)/14.0;
+                    }   
                 }
-                    
+
+                if(inputTask !== "...task" ) SET_COLOR("#8affa9");
+                console.log(inputTime)
+                if(inputEff >= 7) SET_EFF_COLOR("#8affa9");
+                else if(inputEff >= 4) SET_EFF_COLOR("#ffbd7a")
+                else SET_EFF_COLOR("#ff7a7a")
+
+                prevData.eff = eff;
+                props.updateEff(eff);
+                await AsyncStorage.setItem(key, JSON.stringify(prevData));
+            }else{
+                await AsyncStorage.setItem(key, JSON.stringify({ eff: 0, inputTime : { task: inputTask, eff: inputEff } }));
             }
-            prevData.eff = eff;
-            await AsyncStorage.setItem(key, JSON.stringify(prevData));
-            // prevData.
-            // console.log(prevData);
-        }else{
-            await AsyncStorage.setItem(key, JSON.stringify({ eff: 0, inputTime : { task: inputTask, eff: inputEff } }));
         }
-    }
+        asuncFunction();
+    },[inputEff, inputTask]);
 
     return(
         <View style={styles.view}>
-            <Text style={styles.inputTime}> {inputTime} </Text>
+            <Text style={{
+                height: 40,
+                margin: 5,
+                borderWidth: 1,
+                borderRadius: 5,
+                width: 60,
+                padding: 10,
+                textAlign: 'center',
+                backgroundColor: COLOR,
+            }}> {inputTime} </Text>
             <TextInput 
             style={styles.inputTask}
             onChangeText={onChangeinputTask}
@@ -59,15 +89,19 @@ export default function CustomTextInput(props){
             />
             <TextInput 
             keyboardType="numeric"
-            style={styles.inputEff}
+            style={{
+                height: 40,
+                margin: 5,
+                borderWidth: 1,
+                borderRadius: 5,
+                padding: 10,
+                textAlign: 'center',
+                backgroundColor: EFF_COLOR
+            }}
             onChangeText={onChangeinputEff}
+            maxLength={1}
             value={inputEff} 
             />
-            <Pressable style={styles.submit} onPress={onPressDone} > 
-                <Text style={{fontSize: 30}}>
-                ✓
-                </Text> 
-            </Pressable>
         </View> 
     )
 }
@@ -87,6 +121,7 @@ const styles = StyleSheet.create({
         width: 60,
         padding: 10,
         textAlign: 'center',
+        // backgroundColor: COLOR,
     },
     inputTask: {
         height: 40,
@@ -103,6 +138,7 @@ const styles = StyleSheet.create({
         borderRadius: 5,
         padding: 10,
         textAlign: 'center',
+        // backgroundColor: EFF_COLOR
     },
     submit: {
         height: 40,
--- screens/Today.tsx
+++ screens/Today.tsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import { StyleSheet, ScrollView } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 
 // import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
@@ -14,22 +16,61 @@ let timeArray =  [
   '20:00', '21:00',
   '22:00', '23:00'
 ];
-
+/**
+ * Used to parse the date string for suitable representable format.
+ * 
+ * @param {string} dateString
+ * @return {*} 
+ */
 function getDateFromDateString(dateString: string){
   let date = dateString.split("T")[0];
   date = date.substr(1, date.length-1);
   return date;
 }
-
+/**
+ *
+ *
+ * @export
+ * @return {*} 
+ */
 export default function TabOneScreen() {
-  let date = getDateFromDateString(JSON.stringify(new Date()));
+  const [eff, setEff] = React.useState(10);
+  const [date, setDate] = React.useState(getDateFromDateString(JSON.stringify(new Date())));
+  const [ EFF_COLOR, SET_EFF_COLOR ] = React.useState("#ff7a7a");
+  const updateEff = React.useCallback((eff) => {
+    setEff(Math.round(eff * 100) / 100);
+  },[])
+  
+  React.useEffect(() => {
+    /**
+     * This function is used to do asuncronous operation inside the
+     * UseEffect Hook. Without doing so, was fetching me an error.
+     */
+    const foo = async () => {
+      let data;
+      data = await AsyncStorage.getItem(date);
+      data = JSON.parse(data);
+      let eff = Math.round(data.eff * 100) / 100
+      setEff(eff);
+      if(eff >= 5) SET_EFF_COLOR("#8affa9");
+      else if(eff >= 4) SET_EFF_COLOR("#ffbd7a")
+      else SET_EFF_COLOR("#ff7a7a")
+
+    };
+    foo();
+  },[date, eff])
 
   return (
     <View style={styles.container}>
       <Text style={styles.title}>{date}</Text>
+      <Text style={{
+        backgroundColor: EFF_COLOR,
+        padding: 5,
+        borderRadius: 5,
+      }}>Time Efficieny : {eff}</Text>
       <ScrollView>
         {timeArray.map((time) => {
-          return <CustomInput key={time} time={time} date={date}/>
+          return <CustomInput key={time} time={time} date={date} updateEff={updateEff}/>
         })}
       </ScrollView>
     </View>
@@ -39,6 +80,7 @@ export default function TabOneScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    // flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'flex-start',
   },
